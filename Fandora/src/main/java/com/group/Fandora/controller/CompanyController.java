7c0a53a7a56e043ef5dab1fb80dda34002148e85
JiminLim <jimin0190053@gmail.com>
2025-05-02 15:11:02 +0900

    Feat: retrieve company list

--- Fandora/src/main/java/com/group/Fandora/controller/CompanyController.java
+++ Fandora/src/main/java/com/group/Fandora/controller/CompanyController.java
@@ -1,11 +1,15 @@
 package com.group.Fandora.controller;
 
 import com.group.Fandora.dto.CompanyDetailResponse;
+import com.group.Fandora.dto.CompanyListResponse;
 import com.group.Fandora.entity.Company;
 import com.group.Fandora.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/companies")
@@ -26,6 +30,21 @@ public class CompanyController {
                 company.getDescription(),
                 company.getCompanyImage()
         );
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CompanyListResponse>> getAllCompanies() {
+        List<Company> companies = companyRepository.findAll();
+        List<CompanyListResponse> response = companies.stream()
+                .map(company -> new CompanyListResponse(
+                        company.getCompanyId(),
+                        company.getCompanyImage(),
+                        company.getCompanyName(),
+                        company.getDescription()
+                ))
+                .collect(Collectors.toList());
 
         return ResponseEntity.ok(response);
     }
