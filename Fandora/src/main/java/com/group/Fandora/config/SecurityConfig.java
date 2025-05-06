c339b97d79712af00984d425938140846209ae23
JiminLim <jimin0190053@gmail.com>
2025-05-06 18:49:45 +0900

    feat: add Dockerfile and JWT filter

--- Fandora/src/main/java/com/group/Fandora/config/SecurityConfig.java
+++ Fandora/src/main/java/com/group/Fandora/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.group.Fandora.config;
 
+import com.group.Fandora.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -22,7 +30,8 @@ public class SecurityConfig {
                                 "/companies/*/reviews"
                         ).permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
